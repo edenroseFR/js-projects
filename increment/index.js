2a77b6c99f789a2cc96dbfb1a80dff15cbea7d64
edenroseFR <edenroserisma13@gmail.com>
2022-02-28 16:19:39 +0800

    Add more feature on increment

--- increment/index.js
+++ increment/index.js
@@ -1,5 +1,8 @@
 let count = 0;
 let counter = document.getElementById("count");
+let entries = document.getElementById("entry")
+
+
 function increment() {
     count += 1
     let newVal = count.toString()
@@ -10,3 +13,9 @@ function decrement() {
     let newVal = count.toString()
     counter.innerText = newVal
 };
+
+function save(){
+    entries.textContent += counter.innerText + " - "
+    count = 0
+    counter.innerText = "0"
+}
