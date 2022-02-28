88dbbe878d0b33cccd834ae935b96cc255c50e7c
edenroseFR <edenroserisma13@gmail.com>
2022-02-28 16:20:01 +0800

    Update countdown

--- countdown/index.js
+++ countdown/index.js
@@ -1,9 +1,11 @@
+let screenCount = document.getElementById("screen-count")
+let entries = document.getElementById("entry")
+
 function sleep(ms) {
     return new Promise(resolve => setTimeout(resolve, ms));
 };
 
 async function countdown() {
-    let screenCount = document.getElementById("screen-count")
     for (let i=5; i>-1; i--) {
         screenCount.innerText = i.toString()
         await sleep(i * 1000);
